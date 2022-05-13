0cc86fd8bf43dd4d0c7a6d39e213bb0e8a4d3fd8
bhaskar966 <86063625+bhaskar966@users.noreply.github.com>
2022-05-13 08:27:45 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/bhaskar966/android_vendor_xiaomi_sweet.git -b arrow-12.1 vendor/xiaomi/sweet
 
 # Kernel & Clang
-git clone https://github.com/bhaskar966/kernel_xiaomi_sm6150.git -b a12.1oss kernel/xiaomi/sweet
+git clone https://github.com/bhaskar966/kernel_xiaomi_sm6150.git -b s12.1oss kernel/xiaomi/sweet
 git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
